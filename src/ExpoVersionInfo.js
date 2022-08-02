ec3505e819de58c29f56a98c0c21a1ee37999391
Levi Melamed <levimelamed@gmail.com>
2022-08-01 19:27:41 -0500

    clean up

--- src/ExpoVersionInfo.js
+++ src/ExpoVersionInfo.js
@@ -1,38 +1,34 @@
 import { Platform } from 'react-native';
 import Constants from 'expo-constants';
 
-let RNVersionCheck;
 const { manifest = {} } = Constants;
-  const {
+const {
     version = null,
     android: { versionCode = null, package: androidPackageName = null } = {},
     ios: { bundleIdentifier = null, buildNumber = null } = {},
-  } = manifest;
-  let country;
-  
-  RNVersionCheck = {
+} = manifest;
+
+const = RNVersionCheck = {
     currentVersion: version,
-    country,
     currentBuildNumber: Platform.select({
-      android: versionCode,
-      ios: buildNumber,
+        android: versionCode,
+        ios: buildNumber
     }),
     packageName: Platform.select({
-      android: androidPackageName,
-      ios: bundleIdentifier,
+        android: androidPackageName,
+        ios: bundleIdentifier
     }),
-  };
+};
 
-const COUNTRY = RNVersionCheck.country;
 const PACKAGE_NAME = RNVersionCheck.packageName;
 const CURRENT_BUILD_NUMBER = RNVersionCheck.currentBuildNumber;
 const CURRENT_VERSION = RNVersionCheck.currentVersion;
 
 export default function(country) {
-	return {
-		getCountry: () => Promise.resolve(country),
-		getPackageName: () => PACKAGE_NAME,
-		getCurrentBuildNumber: () => CURRENT_BUILD_NUMBER,
-		getCurrentVersion: () => CURRENT_VERSION,
-	};
+    return {
+        getCountry: () => Promise.resolve(country),
+        getPackageName: () => PACKAGE_NAME,
+        getCurrentBuildNumber: () => CURRENT_BUILD_NUMBER,
+        getCurrentVersion: () => CURRENT_VERSION,
+    };
 };
